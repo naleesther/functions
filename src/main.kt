8759a3c72cfa21a314f4b47bc22d0797705a38ba
aluochvivian <aluochviviann@gmail.com>
2022-03-15 11:15:19 +0300

    first commit

---
+++ src/main.kt
@@ -0,0 +1,26 @@
+fun main(){
+    println("Hello Esther")
+    var n=getRemainder(36,7)
+    println(n)
+    var x=Additional(10,10,10,10)
+    println(x)
+    Statement("dancing")
+
+}
+
+
+fun printName(name:String){
+    var name="Hello $name"
+}
+fun getRemainder(num1:Int,num2:Int):Int{
+    var num=num1%num2
+    return num
+}
+fun Additional(a:Int,b:Int,c:Int,d:Int):Int{
+    var num=a+b+c+d
+    return num
+}
+fun Statement(fact:String){
+    println("i enjoy $fact")
+
+}
